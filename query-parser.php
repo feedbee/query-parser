709834fe20a98a31f76bbf1da7a24273eccaa26e
Valera Leontyev <feedbee@gmail.com>
2013-07-06 22:00:53 +0300

    Refactored: parser out of Expression, full tree compilation without recursion

--- query-parser.php
+++ query-parser.php
@@ -1,8 +1,100 @@
 <?php
 
+class Parser
+{
+	public static function parse($string)
+	{
+		$stack = array();
+		$stackExpr = array();
+		$string = self::adapt($string);
+		$expression = new ContainerExpression;
+		$expression->addChild(new StringExpression());
+
+		for ($i = 0; $i < mb_strlen($string); $i++) {
+			$char = mb_substr($string, $i, 1);
+
+			if (count($stack) > 0 && end($stack) == '"' && $char != '"') {
+				$expression->getLastChild()->appendString($char);
+			} else {
+				switch ($char) {
+					case ' ':
+						$expression->addChild(new StringExpression());
+						break;
+					case '"':
+						if (count($stack) > 0 && end($stack) == '"') {
+							// закрываем фразу
+							$expression->addChild(new StringExpression);
+							array_pop($stack);
+						} else {
+							// открываем фразу
+							$expression->addChild($ph = new Phrase);
+							array_push($stack, $char);
+						}
+						break;
+					case '(':
+						// открываем выражение
+						$expression->addChild($cnt = new ContainerExpression);
+						$cnt->addChild(new StringExpression);
+						array_push($stack, ')');
+						array_push($stackExpr, $expression);
+						$expression = $cnt;
+						break;
+					case ')':
+						if (count($stack) > 0 && end($stack) == ')') {
+							// есть что закрыть — закрываем выражение
+							$expression = array_pop($stackExpr);
+							$expression->addChild(new StringExpression);
+							array_pop($stack);
+						} else {
+							// скобка "не в тему"
+							$expression->getLastChild()->appendString($char);
+						}
+						break;
+					default:
+						$expression->getLastChild()->appendString($char);
+						break; 
+				}
+			}
+		}
+
+		if (count($stackExpr) > 0) {
+			return $stackExpr[0];
+		}
+
+		return $expression;
+	}
+
+	private static function adapt($string)
+	{
+		return preg_replace('/\s+/', ' ', $string); // replace any count of any space characted with single space
+	}
+}
+
 class Expression {
-	private $string = "";
+
+}
+
+class ContainerExpression extends Expression {
 	private $childNodes = array();
+
+	public function addChild(Expression $expression)
+	{
+		$this->childNodes[] = $expression;
+	}
+
+	public function getLastChild()
+	{
+		return end($this->childNodes);
+	}
+
+	public function __toString()
+	{
+		return implode(' ', $this->childNodes);
+	}
+}
+
+class StringExpression extends Expression {
+	private $string = "";
 
 	public function __construct($string = "")
 	{
@@ -18,106 +110,18 @@ class Expression {
 	{
 		return $this->string;
 	}
-
-	public function parse()
-	{
-		$stack = array();
-		$string = self::adapt($this->string);
-		$nodes = array(new Expression);
-
-		for ($i = 0;  $i < mb_strlen($string); $i++) {
-			$char = mb_substr($string, $i, 1);
-
-			switch ($char) {
-				case ' ':
-					if (count($stack) == 1) { // только 1-й уровень
-						$nodes[] = new Expression();
-					} else {
-						end($nodes)->appendString($char);
-					}
-					break;
-				case '"':
-					if (count($stack) > 0 && end($stack) == '"') {
-						// закрываем фразу
-						if (count($stack) == 1) { // только 1-й уровень
-							$nodes[] = new Expression;
-						} else {
-							end($nodes)->appendString($char);
-						}
-						array_pop($stack);
-					} else {
-						// открываем фразу
-						if (count($stack) == 0) { // только 1-й уровень
-							$nodes[] = new Phrase;
-						} else {
-							end($nodes)->appendString($char);
-						}
-						array_push($stack, $char);
-					}
-					break;
-				case '(':
-					// открываем выражение
-					if (count($stack) == 0) { // только 1-й уровень
-						$nodes[] = new Expression;
-					} else {
-						end($nodes)->appendString($char);
-					}
-					array_push($stack, ')');
-					break;
-				case ')':
-					if (count($stack) > 0 && end($stack) == ')') {
-						// есть что закрыть — закрываем выражение
-						if (count($stack) == 1) { // только 1-й уровень
-							end($nodes)->parse();
-							$nodes[] = new Expression;
-						} else {
-							end($nodes)->appendString($char);
-						} 
-						array_pop($stack);
-					} else {
-						// скобка "не в тему"
-						end($nodes)->appendString($char);
-					}
-					break;
-				default:
-					end($nodes)->appendString($char);
-					break; 
-			}
-		}
-
-		// незакрытые парные элементы
-		while (count($stack) > 0) {
-			$char = array_pop($stack);
-			$this->appendString($char);
-			if (count($stack) == 0 && $char == ")") {
-				end($nodes)->parse();
-			}
-		}
-
-		$this->childNodes = $nodes;
-	}
-
-	private static function adapt($string)
-	{
-		return preg_replace('/\s+/', ' ', $string); // replace any count of any space characted with single space
-	}
 }
 
-class Compiled extends Expression {}
-
-class Literal extends Expression {}
-
-class Phrase extends Literal {}
+class Phrase extends StringExpression {}
 
 class Operator extends Expression {
 	private $priority = 0;
 }
 
-$e = new Expression('проверка  трех слов');
-$e = new Expression('проверка (трех с половиной) слов');
-$e = new Expression('-проверка трех -слов');
-$e = new Expression('(проверка (трех "1""2)")слов');
-//$e = new Expression('O ("A" "B)" D');
-//$e = new Expression('проверка  (((трех))) "слов');
-$e->parse();
+$e = Parser::parse('проверка  трех слов');
+$e = Parser::parse('проверка (трех с половиной) слов');
+$e = Parser::parse('-проверка трех -слов');
+$e = Parser::parse('(проверка (трех "1""2)")слов');
+$e = Parser::parse('O ("A" "B)" D');
+$e = Parser::parse('проверка  (((трех))) "слов');
 var_dump($e);
